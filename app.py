0dbb538a6877938182706129cee90bd103c55643
fedizran41 <fedizran41@gmail.com>
2026-02-04 19:23:37 +0100

    deploiement

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ if uploaded_file is not None and model is not None:
     col1, col2 = st.columns(2)
 
     with col1:
-        st.image(img, caption="Image à analyser", use_container_width=True)
+        st.image(img, caption="Image à analyser")
 
     with col2:
         if st.button("Lancer le diagnostic"):
